853780b61ac563cdd692f608d718d04b6726efc0
Vpiete59 <Virgile.pieterwas@gmail.com>
2025-11-26 03:59:21 +0100

    Logo Kinevir vraiment triple

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
                 priority
               />
             </div>
-            <div className="relative h-12 w-40 sm:h-14 sm:w-48 flex-shrink-0">
+            <div className="relative h-16 w-56 sm:h-20 sm:w-72 flex-shrink-0">
               <Image
                 src="/logo-titre.webp"
                 alt="Kinevir"
